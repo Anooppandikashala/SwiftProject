2133eeccb9b8b72bfef6a3aeaec95dc66a01c4af
ANOOP P <anooppkrishnan96@gmail.com>
2021-03-05 15:52:30 +0000

    Inital Commit

--- main.swift
+++ main.swift
@@ -1,12 +1,55 @@
 import Foundation;
 
+struct Question
+{
+  let question:String ;
+  let answer:String;
+  let options:Set<String>;
 
+  init(question:String, answer:String, options:Set<String>) 
+  {
+    self.question = question;
+    self.answer = answer;
+    self.options = options;
+  }
+
+}
 
 class QuestionBank
 {
-  var easyQuestions:Set=[];
-  var mediumQuestions:Set=[];
-  var hardQuestions:Set=[];
+  var easyQuestions:Array<Question>=[];
+  var mediumQuestions:Array<Question>=[];
+  var hardQuestions:Array<Question>=[];
+
+  func getEasyQuestions() -> Array<Question>
+  {
+    return easyQuestions;
+  }
+
+  func getMediumQuestions() -> Array<Question>
+  {
+    return mediumQuestions;
+  }
+
+  func getHardQuestions() -> Array<Question>
+  {
+    return hardQuestions;
+  }
+
+  func initializeEasyQuestions()
+  {
+
+  }
+
+  func initializeMediumQuestions()
+  {
+    
+  }
+
+  func initializeHardQuestions()
+  {
+    
+  }
 
 
 };
