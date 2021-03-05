54ee520869fccd01ad4c9ccf386932176eea2abf
ANOOP P <anooppkrishnan96@gmail.com>
2021-03-05 15:27:25 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,33 @@
+import Foundation;
+
+
+
+class QuestionBank
+{
+  var easyQuestions:Set=[];
+  var mediumQuestions:Set=[];
+  var hardQuestions:Set=[];
+
+
+};
+
+class Game
+{
+  func start()
+  {
+
+  }
+
+};
+
+
+
+func main()
+{
+
+  let myGame = Game();
+  myGame.start();
+
+}
+
+main()
